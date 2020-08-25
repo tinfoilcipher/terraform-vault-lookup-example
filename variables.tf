639433fc6b68acd5e092843e69789f49b3210c60
tinfoilcipher <39910469+tinfoilcipher@users.noreply.github.com>
2020-08-25 22:30:01 +0100

    v12 syntax upgrade

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 #--Azure Location
 variable "location" {
     description = "Primary Azure Location"
-    type        = "string"
+    type        = string
     default     = "eastus"
 }
 
 # Vault URL
 variable "vault_url" {
     description = "Vault URL"
-    type        = "string"
+    type        = string
     default     = "https://<your_vault_ip_or_fqdn>:8200"
 }
 
 # Vault Token (For Runtime Input)
 variable "vault_token" {
-    type        = "string"
+    type        = string
     description = "Vault Token"
 }
 
 #--Resource Group
 variable "resource_group" {
     description = "All resource groups"
-    type        = "string"
+    type        = string
     default     = "tinfoil_database_rg"
 }
 
 #--Database Server
 variable "sql_database_server" {
     description = "Database Server"
-    type        = "string"
+    type        = string
     default     = "tinfoil-database-server"
 }
 
